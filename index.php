1ec6c0be48284e214563ac2268ea44d830873ecd
scruffy21 <yougotmebeggingyouformercy@gmail.com>
2018-03-25 13:42:48 +0200

    added encoding

--- index.php
+++ index.php
@@ -54,8 +54,9 @@ function fillData($name, $website, $comment, $photo, $flag, $country, $time, $da
 ?>
 <!DOCTYPE html>
 
-<html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Maven+Pro|Roboto" rel="stylesheet">
